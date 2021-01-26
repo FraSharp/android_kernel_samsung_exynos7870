5b18690765bbe10a7c704cbf0ef1c0e2b7aa8454
Francesco <60934657+FraSharp@users.noreply.github.com>
2021-01-26 22:18:17 +0100

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,6 +2,7 @@
 
 export ARCH=arm64
 export CROSS_COMPILE=/home/f_duca00/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
+export CONFIG_CROSS_COMPILE=/home/f_duca00/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
 export ANDROID_PLATFORM_VERSION=10
 
