beb1ed2a41bc063e0e366ba061f60d147cbb4fdf
Francesco <60934657+FraSharp@users.noreply.github.com>
2021-01-27 09:17:49 +0100

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,8 +3,9 @@
 export ARCH=arm64
 export CROSS_COMPILE=/home/f_duca00/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
 export CONFIG_CROSS_COMPILE=/home/f_duca00/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
-export ANDROID_MAJOR_VERSION=q
-export ANDROID_PLATFORM_VERSION=10
+export ANDROID_MAJOR_VERSION=r
+export ANDROID_PLATFORM_VERSION=11
 
 make O=./out exynos7870-j6lte_defconfig
-make O=./out -j64
+make O=./out -j64 \
+             CC=clang
